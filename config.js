b66777d9fd172652a7dc91bfa14079604a4e84ac
Phongly59 <89494871+Phongly59@users.noreply.github.com>
2021-09-08 18:42:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Nóc Nhà à',
+    introDesc: `A muốn em trả lời câu hỏi này?:3`,
+    btnIntro: 'Dạaaaaa',
+    title: 'Em bé của anh có yêu anh không?',
+    desc: 'đáp án ở bên trái ấy',
+    btnYes: 'E bé yêu anh rất nhiều :3',
+    btnNo: 'không yêu',
+    question:'A bt là em yêu anh mà(a cố tình đáy hihi)',
+    btnReply: 'nhắn những lời yêu thương cho a nào.',
+    reply: 'em rất là yeeuuu anh luoonnn <33333',
+    mess: 'Anh bt goy hí hí',
+    messDesc: 'Chúc mừng sinh nhật vui vẻ nhé công chúa của a :3,nhắn tin với anh nhá',
+    btnAccept: 'dzaaaaaaa',
+    messLink: 'http://www.facebook.com/profile.php?id=100042213502158
 }
